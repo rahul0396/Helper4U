c693fe45b0b040ec191ba362d33f193703ef0fb5
Mahakchhipa <mehekjajpuraom700@gmail.com>
2024-09-25 17:07:21 +0530

    setup project

---
+++ Helper-For-You/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+        <>
+         <a href="#"> <img src="/src/img/logo.png" alt="logo" height={"50px"}/></a>
+         <ul>
+            <li>Home</li>
+            <li>Hiring</li>
+            <li>Verfication</li> 
+         </ul>
+         <button> Login</button>
+        </>
+    )
+}
+
+export default Header;
